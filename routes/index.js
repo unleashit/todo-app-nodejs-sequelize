bc992851d08e55bee55dd8d3b6bbfe33849720f3
Jason Gallagher <unleashit@yahoo.com>
2016-04-25 18:34:21 -0700

    working todo app

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var models  = require('../models');
+var express = require('express');
+var router  = express.Router();
+
+var bodyParser = require('body-parser');
+router.use(bodyParser.urlencoded({ extended: true }));
+
+router.get('/', function(req, res) {
+  models.User.findAll({
+    include: [ models.Task ]
+  }).then(function(users) {
+    res.render('index', {
+      title: 'Express',
+      users: users
+    });
+  });
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,49 @@
+var models  = require('../models');
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/create', function(req, res) {
+  models.User.create({
+    username: req.body.username
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+router.get('/:userid/destroy', function(req, res) {
+  models.User.destroy({
+    where: {
+      id: req.params.userid
+    }
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+router.post('/:userid/tasks/create', function(req, res) {
+  console.log('dinosaur');
+  models.Task.create({
+      title: req.body.title,
+      UserId: req.params.userid
+  }).then(function() {
+    res.redirect('/');
+  });
+});
+
+router.get('/:userid/tasks/:taskid/destroy', function(req, res) {
+  models.Task.destroy({
+    where: {
+      id: req.params.taskid
+    }
+  }).then(function(success, err) {
+    if (err) console.log(err);
+    res.redirect('/');
+  });
+});
+
+module.exports = router;
